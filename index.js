078a716973072ae87c1dd4cd4108216cd2c7a43d
Donatus Raphael Nelson <raphaeldonatus9@gmail.com>
2021-04-07 23:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var greeting = "Hello, everyody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello," + " "+ specialGuest + "!";
 
-var topic = "let\'s talk about let\'s talk about space";
+var topic = "let\'s talk about let\'s talk about space.";
 
 var conversation = `${topic}`;
+console.log(conversation);
